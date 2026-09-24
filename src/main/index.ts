671f0ea76af3df7488c8de0c4dd602e481eae28c
PabloDev19 <placenciapablo.oficial@gmail.com>
2026-09-24 01:58:08 -0500

    Change app dimensions

--- src/main/index.ts
+++ src/main/index.ts
@@ -6,8 +6,8 @@ import icon from '../../resources/icon.png?asset'
 function createWindow(): void {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 850,
+    width: 1150,
+    height: 750,
     show: false,
     resizable: false,
     autoHideMenuBar: true,
