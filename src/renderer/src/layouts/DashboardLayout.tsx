a69811160a715d55bb67646a3bb8ec23d581f61d
PabloDev19 <placenciapablo.oficial@gmail.com>
2026-09-23 23:03:49 -0500

    Fix error border-context

--- src/renderer/src/layouts/DashboardLayout.tsx
+++ src/renderer/src/layouts/DashboardLayout.tsx
@@ -1,9 +1,33 @@
+import Logo from '../assets/mova-stream-logo.png'
 import './layout.css'
 
 export const DashboardLayout = ({}) => {
   return (
     <section className="dashboard_layout">
-      <article className="aside_menu"></article>
+      <article className="aside_menu">
+        <div className="logo">
+          <img src={Logo} alt="Logo Mova Stream Lab" />
+          <p className="title">Mova Stream Lab</p>
+        </div>
+
+        <nav className="navigation">
+          <a className="link active" href="#">
+            Inicio
+          </a>
+          <a className="link" href="#">
+            Comandos
+          </a>
+          <a className="link" href="#">
+            Eventos
+          </a>
+          <a className="link" href="#">
+            Economia
+          </a>
+          <a className="link" href="#">
+            Configuración
+          </a>
+        </nav>
+      </article>
       <article className="render_view"></article>
     </section>
   )
